83a0f9a67f6b12d30bb04d618016a84416ed1ff4
RonGo201 <163910657+RonGo201@users.noreply.github.com>
2026-08-03 14:09:19 +0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = {}
+        
+        for word in strs:
+            char_count = [0] * 26  
+            for char in word:
+                index = ord(char) - ord('a')
+                char_count[index] += 1
+
+            dict_key = tuple(char_count)
+
+            if dict_key not in anagram_map:
+                anagram_map[dict_key] = []
+
+            anagram_map[dict_key].append(word)
+
+        res = []
+        for key, words in anagram_map.items():
+            res.append(words)
+        
+        return res
